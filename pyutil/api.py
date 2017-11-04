25ee98ea09b4f88d0536da30bb94eed076136b48
Zheng Luo <helloworld09@163.com>
2017-11-04 16:29:46 +0800

    python api crawler

---
+++ pyutil/api.py
@@ -0,0 +1,88 @@
+import requests
+
+
+
+def nlu(text):
+    api_url = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze"
+    auth = ('48ecf36f-4359-4dcf-87ac-99480df20c98', 'dRGCkJQGzY7n')
+    data = {
+        "text": text,
+        "version": "2017-02-27",
+        "features": "sentiment,keywords",
+        "keywords.sentiment": "true"
+    }
+
+    ret = requests.get(api_url, params=data, auth=auth)
+    return ret.json()
+
+def text2speech(text):
+    api_url = "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
+    auth = ("633214e0-75dc-41dc-b70c-051335b92583", "Z8FxXqbe3fAw")
+    data = {
+        "text": text,
+    }
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'audio/wav',
+    }
+    ret = requests.post(url=api_url, data=data, headers=headers, auth=auth)
+    return ret.status_code
+
+def speech2text(audioFilename):
+    api_url = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize'
+    auth = ("649b5fa7-2fc9-4a03-ba51-936d66ffdd0b", "dI7PZXqM7ejt")
+    headers = {
+        'Content-Type': 'audio/wav',
+    }
+    data = open(audioFilename, 'rb').read()
+    ret = requests.post(api_url, headers=headers, data=data, auth=auth)
+    return ret.json()
+
+def classifyImage(imageFilename):
+    api_url = 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify'
+    api_key = '6d6f0991070b739eff2c015cb7193f88e28896e9'
+
+    params = (
+        ('api_key', api_key),
+        ('version', '2016-05-20'),
+    )
+    data = open(imageFilename, 'rb').read()
+
+    ret = requests.post(api_url, params=params, data=data)
+    return ret.json()
+
+def detectingFaces(imageFilename):
+    api_url = 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/detect_faces'
+    api_key = '6d6f0991070b739eff2c015cb7193f88e28896e9'
+
+    params = (
+        ('api_key', api_key),
+        ('version', '2016-05-20'),
+    )
+    data = open(imageFilename, 'rb').read()
+    ret = requests.post(api_url, params=params, data=data)
+    return ret.json()
+
+def personalInsight(text):
+    api_url = 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile'
+    auth = ('cc9a4bfe-9c09-4407-8618-d18ef6271fbd', 'jy7op5MHFDbi')
+
+    headers = {
+        'Content-Type': 'text/plain;charset=utf-8',
+    }
+
+    params = (
+        ('version', '2017-10-13'),
+    )
+    # data = open(textFilename, 'r', encoding='utf-8').read()
+    ret = requests.post(api_url, params=params, data=text, headers=headers, auth=auth)
+
+    return ret.json()
+
+
+if __name__ == '__main__':
+    # print(nlu('Understand the contents of images. Create custom classifiers to develop smart applications'))
+    # print(speech2text('hello_world.wav'))
+    # print(classifyImage('test.jpg'))
+    # print(detectingFaces('test.jpg'))
+    print(personalInsight("En Grecia 🇬🇷, ¡mañana vuelve la UEFA Champions League\nDesayunando con ellos.\nPartido difícil en San Mamés. Tuvimos que pelear muchísimo para ganar hoy a un gran rival. \nHoy estuvimos en Londres para los 'The Best FIFA Football Awards2017'. Felicidades a todos los ganadores.\n¡Era importante sumar 3 puntos más hoy y lo logramos!\nEn la App podés acompañarme en todos mis partidos. ¿Ya la tenés?\nOtro paso más hacia adelante en la Champions.\nArrancamos la semana preparando nuestro próximo partido de UEFA Champions League.\nOrgulloso de ser embajador para Tour n Cure, un programa global que arrancó en Egipto para erradicar la #HepatitisC1# del mundo. Gracias al trabajo realizado hasta hoy se han curado más de un millón de personas, las cuales llevaban más de dos años esperando el tratamiento para es ​​​​..".encode('utf-8')))
---
+++ pyutil/weiboCrawler.py
@@ -0,0 +1,69 @@
+# encoding=utf-8
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+user_id = 2348648143
+# user_id=1669879400
+cookie = {
+    "Cookie": "SCF=AiRAchUSpkqXd7eCfjSUvWDx7x-SA9m6ggC3JdCT7hdfRHpypezI9uy25nmdGHBxazqHJrWZpiCuPNI1XskQMBU.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5Yu6VdSKJ9vm8rP7elu0UM5JpX5K-hUgL.Fo-N1h271h-ESK22dJLoI7DGdsvEwH8fqgLj; _T_WM=b5bc6005f2a81fa5f26c0ee44ec80293; SUB=_2A250-QacDeRhGeNJ41MR-CvOzj2IHXVUBarUrDV6PUJbkdBeLRfwkW1-pSi5PQl5M2o0HYB9FzSzrezpvg..; SUHB=0nluE29Qcq629A; SSOLoginState=1509783244"}
+pages = 5
+for page in range(pages):
+    url = 'http://weibo.cn/u/%d?page=%d&filter=0' % (user_id, page)
+    path = str(user_id)
+    html = requests.get(url, cookies=cookie)
+
+    Soup = BeautifulSoup(html.text, 'html.parser')
+
+    alla = Soup.find_all("div", class_="c")  # 所有的微博
+
+    for a in alla:
+        per = a.find("span", class_="ctt")  # 每条微博
+
+        if per:
+            for a_tag in per("a"):
+                a_tag.extract()
+            print(per.text)
+            f = open(path + '.txt', 'a+', encoding='utf-8')
+            f.write(per.text + "\n")
+            #    regular1=re.compile(r"\d+")
+            #    pgs = regular1.findall(per.text)  #每条微博对应的评论数
+
+            #    href=per['href']  #每条微博对应的评论链接
+            #    comments = requests.get(href,cookies=cookie)
+            #    soup = BeautifulSoup(comments.text, 'lxml')
+            #    comment = soup.find_all("div",class_="c")#评论链接里评论的div
+
+
+            #    for cmt in comment:
+
+            #      content = cmt.find("span",class_="ctt")   #评论内容
+
+            #      time = cmt.find("span",class_="ct")
+            #      if (content):
+            #          name="comment"
+            #          f = open(name+'.txt', 'a+',encoding="utf-8")
+            #          f.write(time.text[:19]+"     "+content.text+"\n")
+            #         #print(content.text,time.text[:19])
+
+            #    if(int(pgs[0])>10):              #一条微博的评论数大于10，即有多页
+            #       regular2=re.compile(r"\d+")
+            #       pg=soup.find("div",class_="pa").find("div")
+
+            #       pagenum=regular2.findall(pg.text)[1]   #抓取页数
+            #       for i in range(2,int(pagenum)+1):
+            #          newhref=per['href'][:-7]+"&page=%d"%(i)    #第i页的url
+            #          newcomments = requests.get(newhref,cookies=cookie)
+            #          newsoup = BeautifulSoup(newcomments.text, 'lxml')
+            #          newcomment = newsoup.find_all("div",class_="c")
+
+            #          for newcmt in newcomment:
+
+            #             newcontent = newcmt.find("span",class_="ctt")
+
+            #             newtime = newcmt.find("span",class_="ct")
+            #             if (newcontent):
+            #               name="comment"
+            #               f = open(name+'.txt', 'a+',encoding="utf-8")
+            #               f.write(newtime.text[:19]+"     "+newcontent.text+"\n")
